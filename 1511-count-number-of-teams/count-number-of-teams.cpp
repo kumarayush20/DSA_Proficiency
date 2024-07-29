8db9273d64ef5b765fd2cf81c0032cb559592bc7
kumarayush20 <114385386+kumarayush20@users.noreply.github.com>
2024-07-29 14:59:31 +0530

    Time: 29 ms (74.39%) | Memory: 11.7 MB (40.04%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -1,62 +1,30 @@
+/* 
+1 more logic to apply is before i and after i, jitne satisfy krenge condition, utne ka multiplication is gonna be added to ans,
+to calculate the permutations.
+*/
 class Solution {
 public:
-    int numTeams(vector<int>& ratings) {
-        // Find the maximum rating
-        int maxRating = 0;
-        for (int rating : ratings) {
-            maxRating = max(maxRating, rating);
+    int numTeams(vector<int>& teams) {
+        int n = teams.size();
+        int ans = 0;
+        
+        for (int i = 1; i < n - 1; i++) {
+            int lessLeft = 0, moreLeft = 0;
+            int lessRight = 0, moreRight = 0;
+            
+            for (int j=0; j<i; j++) {
+                if (teams[j] < teams[i]) lessLeft++;
+                if (teams[j] > teams[i]) moreLeft++;
+            }
+            
+            for (int k = i+1; k<n; k++) {
+                if (teams[k] < teams[i]) lessRight++;
+                if (teams[k] > teams[i]) moreRight++;
+            }
+            
+            ans += lessLeft * moreRight + moreLeft * lessRight;
         }
-
-        // Initialize Binary Indexed Trees for left and right sides
-        vector<int> leftBIT(maxRating + 1, 0);
-        vector<int> rightBIT(maxRating + 1, 0);
-
-        // Populate the right BIT with all ratings initially
-        for (int rating : ratings) {
-            updateBIT(rightBIT, rating, 1);
-        }
-
-        int teams = 0;
-        for (int currentRating : ratings) {
-            // Remove current rating from right BIT
-            updateBIT(rightBIT, currentRating, -1);
-
-            // Count soldiers with smaller and larger ratings on both sides
-            int smallerRatingsLeft = getPrefixSum(leftBIT, currentRating - 1);
-            int smallerRatingsRight = getPrefixSum(rightBIT, currentRating - 1);
-            int largerRatingsLeft = getPrefixSum(leftBIT, maxRating) -
-                                    getPrefixSum(leftBIT, currentRating);
-            int largerRatingsRight = getPrefixSum(rightBIT, maxRating) -
-                                     getPrefixSum(rightBIT, currentRating);
-
-            // Count increasing and decreasing sequences
-            teams += (smallerRatingsLeft * largerRatingsRight);
-            teams += (largerRatingsLeft * smallerRatingsRight);
-
-            // Add current rating to left BIT
-            updateBIT(leftBIT, currentRating, 1);
-        }
-
-        return teams;
-    }
-
-private:
-    // Update the Binary Indexed Tree
-    void updateBIT(vector<int>& BIT, int index, int value) {
-        while (index < BIT.size()) {
-            BIT[index] += value;
-            index +=
-                index & (-index);  // Move to the next relevant index in BIT
-        }
-    }
-
-    // Get the sum of all elements up to the given index in the BIT
-    int getPrefixSum(vector<int>& BIT, int index) {
-        int sum = 0;
-        while (index > 0) {
-            sum += BIT[index];
-            index -= index & (-index);  // Move to the parent node in BIT
-        }
-        return sum;
+        
+        return ans;
     }
 };
